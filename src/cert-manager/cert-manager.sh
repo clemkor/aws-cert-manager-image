2dee5e6cb0b669b3445c763101a0e438ffcdac44
Toby Clemson <tobyclemson@gmail.com>
2018-01-01 16:28:02 +0000

    Optionally include public IP in certbot domains.

--- src/cert-manager/cert-manager.sh
+++ src/cert-manager/cert-manager.sh
@@ -3,22 +3,27 @@
 set -e
 set -o pipefail
 
-# Ensure AWS_REGION environment variable is present
 if [ -z "$AWS_REGION" ]; then
   echo >&2 'Error: missing AWS_REGION environment variable.'
   exit 1
 fi
 
-# Ensure AWS_S3_CONFIGURATION_OBJECT environment variable is present
 if [ -z "$AWS_S3_CONFIGURATION_OBJECT" ]; then
   echo >&2 'Error: missing AWS_S3_CONFIGURATION_OBJECT environment variable.'
   exit 1
 fi
 
-# Fetch and source overrides env file
 echo "Fetching and sourcing configuration."
 eval $(aws s3 cp --sse AES256 --region ${AWS_REGION} \
     ${AWS_S3_CONFIGURATION_OBJECT} - | sed 's/^/export /')
+
+echo "Determining certificate domains."
+if [ "${CERT_MANAGER_INCLUDE_PUBLIC_IP}" == "yes" ]; then
+    public_ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
+    domains="${CERT_MANAGER_DOMAIN},${public_ip}"
+else
+    domains="${CERT_MANAGER_DOMAIN}"
+fi
 
 echo "Fetching certificate..."
 certbot certonly \
@@ -32,7 +37,7 @@ certbot certonly \
     --work-dir /opt/cert-manager/work/ \
     --agree-tos \
     --preferred-challenges dns \
-    --domain ${CERT_MANAGER_DOMAIN} \
+    --domain ${domains} \
     --email ${CERT_MANAGER_EMAIL}
 
 echo "Converting to PKCS12 keystore..."
